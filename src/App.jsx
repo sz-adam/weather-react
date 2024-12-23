26509e1b2fe91e9763794ef502b6c180bf7c0346
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2024-12-23 13:22:21 +0100

    add redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Weather from './pages/weather'
+import Weather from './pages/Weather'
+import store from './redux/store'
+import { Provider } from "react-redux";
 
 function App() {
 
   return (
-    <>
+    <Provider store={store}>
      <Weather />
-    </>
+    </Provider>
   )
 }
 
