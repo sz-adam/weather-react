6fe8b319a8e06d2f99d565ad7f773a3a6a91e7d1
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2024-12-27 19:02:05 +0100

    hourly icon day or night

--- src/components/HourlyDisplay.jsx
+++ src/components/HourlyDisplay.jsx
@@ -1,18 +1,23 @@
 import { Box, Typography } from "@mui/material";
-
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import AirIcon from "@mui/icons-material/Air";
 import NightlightIcon from "@mui/icons-material/Nightlight";
-
 import React from "react";
 import { useSelector } from "react-redux";
 
 function HourlyDisplay({ data }) {
   const timeString = data?.time;
   const [date, time] = timeString ? timeString.split("T") : ["N/A", "N/A"];
-  const { weatherData } = useSelector(
+
+  const { weatherData, combinateDailyData } = useSelector(
     (state) => state.search
   );
+
+  const isDaytime = combinateDailyData?.some((day) => {
+    const { sunrise, sunset } = day;
+    return timeString >= sunrise && timeString <= sunset;
+  });
+
   return (
     <Box
       sx={{
@@ -24,13 +29,14 @@ function HourlyDisplay({ data }) {
         minHeight: "300px",
         backgroundColor: "rgba(255, 255, 255, 0.3)",
         boxShadow: "0px 4px 6px 4px rgba(0, 0, 0, 0.6)",
-      }}
-    >
+      }}    >
       <Typography sx={{ padding: "2px", fontSize: "24px" }}>{time}</Typography>
       <Typography sx={{ padding: "2px", fontSize: "14px" }}>{date}</Typography>
-
-      <WbSunnyIcon sx={{ color: "orange", fontSize: "40px" }} />
-
+      {isDaytime ? (
+        <WbSunnyIcon sx={{ color: "orange", fontSize: "40px" }} />
+      ) : (
+        <NightlightIcon sx={{ color: "lightslategray", fontSize: "40px" }} />
+      )}
       <Typography sx={{ padding: "2px" }}>
         {data.temperature_2m} {weatherData.hourly_units.temperature_2m}
       </Typography>
